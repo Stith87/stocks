168b1657ec142e1a51df9eb7886ea4b36266ccaa
Michael Stith <mstith@M-10025604.local>
2014-11-08 14:38:48 -0800

    json output for historical controller

--- app/app/controllers/historical_controller.rb
+++ app/app/controllers/historical_controller.rb
@@ -1,43 +1,16 @@
 class HistoricalController < ApplicationController
 
 	def connect		
-		@symbol = "AAPL"
+		symbol = "AAPL"
+		@data = []
 		Historical.table_name = 'stock_AAPL'
-	 	@historicals = Historical.find_by_sql("SELECT * FROM stock_#{@symbol} ORDER BY date DESC")
-	 	@avg_price = 0
-
-	 	respond_to do |format|
-             format.html # show.html.erb
-             format.json { render json: @historicals} #  
-
-         end 
-
-	 	@averageData = Historical.find_by_sql("SELECT * FROM stock_#{@symbol} ORDER BY date DESC limit 200");
-	 	@avg_prices = 0
-	 	@stock_array = []
-
-
-
 	 	
-	 	@historicals.map do |stock| 
-          	
-          	stock = stock.to_json.delete! ''
-
-            @stock_array << stock
-
+	 	@stocks =Historical.find_each( batch_size: 5000) 
+				
+		 
+	    if request.xhr?
+	      render :json => @stocks
 		end
-
-
-
-		@averageData.each do |stock|
-
-			@avg_prices += stock.Close
-
-		end
-
-		
-
 	end
-
 
 end
